3f1612551b866babe05a8ec9ca28c8b3e23c3860
olac <olia.chezhina@gmail.com>
2022-10-05 20:46:11 +0300

    template to result component

--- src/components/ResultSection.vue
+++ src/components/ResultSection.vue
@@ -1,6 +1,8 @@
-<div class="result">
-    <div class="title">You got sample result 1!</div>
-    <div class="desc">
-        Enter a short description here about the result.
+<template>
+    <div class="result">
+        <div class="title">You got sample result 1!</div>
+        <div class="desc">
+            Enter a short description here about the result.
+        </div>
     </div>
-</div>
+</template>
