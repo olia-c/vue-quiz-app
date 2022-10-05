46f73af6b2aee1818f49e5e4366fc711c5f48463
olac <olia.chezhina@gmail.com>
2022-10-05 20:44:38 +0300

    init questions and results

---
+++ src/components/QuestionList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="questions-ctr">
+        <div class="progress">
+            <div class="bar"></div>
+            <div class="status">1 out of 3 questions answered</div>
+        </div>
+        <div class="single-question">
+            <div class="question">Sample Question 1</div>
+            <div class="answers">
+                <div class="answer">Sample Answer 1</div>
+                <div class="answer">Sample Answer 2</div>
+                <div class="answer">Sample Answer 3</div>
+                <div class="answer">Sample Answer 4</div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/ResultSection.vue
@@ -0,0 +1,6 @@
+<div class="result">
+    <div class="title">You got sample result 1!</div>
+    <div class="desc">
+        Enter a short description here about the result.
+    </div>
+</div>
